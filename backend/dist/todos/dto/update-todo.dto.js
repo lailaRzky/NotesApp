d41c4e0af047569fbb3320f63ed7c3d54747692b
lailaRzky <raichaan98@gmail.com>
2026-07-24 00:13:10 +0700

    Update update-todo.dto.js

--- backend/dist/todos/dto/update-todo.dto.js
+++ backend/dist/todos/dto/update-todo.dto.js
@@ -6,4 +6,3 @@ const create_todo_dto_1 = require("./create-todo.dto");
 class UpdateTodoDto extends (0, mapped_types_1.PartialType)(create_todo_dto_1.CreateTodoDto) {
 }
 exports.UpdateTodoDto = UpdateTodoDto;
-//# sourceMappingURL=update-todo.dto.js.map
